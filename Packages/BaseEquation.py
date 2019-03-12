e7c08fc2080840f60e85ba3dc7fc851b0cf3201e
Benni <benny.schmiedel@gmail.com>
2019-03-12 15:05:26 +0100

    Version 0.1.1 general cleanup of variables and dataassignment, expansion to dictionaries to access elements by name

--- Packages/BaseEquation.py
+++ Packages/BaseEquation.py
@@ -4,12 +4,12 @@
 # In[1]:
 
 
-def BaseEquation(EQparam,funccomp):
-    y=0                                #variable which can be used to sum up functions
-    funclist=funccomp[0]               #Extracting needed arrays from the funccomp array
-    funcparam=funccomp[1]
-    C_ao=EQparam[0]                    #Extracting Equationparameters
+def BaseEquation(eqparam,funccomp):
+    y=0                    	            #variable which can be used to sum up functions
+    funclist=funccomp['funclist']             #Extracting needed arrays from the funccomp array
+    funcparam=funccomp['funcparam']
+    C_ao=eqparam['c_ao']                    #Extracting Equationparameters
     for i in range(len(funclist)):
-            y += funclist[i](funcparam[i])    #Calling the selected function and sum them up 
+            y += funclist['func'+str(i)](funcparam['func'+str(i)])    #Calling the selected function and sum them up 
     return y/C_ao           #output of y, weighted with the heat capacity
 
--- Packages/Variables.py
+++ Packages/Variables.py
@@ -90,99 +90,110 @@ def datareset():
     Vars.Lat2=classreset.Lat2
 
 def Variable_importer(config):
-    EQparam, rk4input, funccomp, var= config
 
-    Builtin_importer(rk4input)
-    Var_importer(var)
-    Output_importer(var)
+    Builtin_importer(config['rk4input'])
+    Var_importer(config['initials'])
+    Output_importer()
 
 def Builtin_importer(rk4input):
 
     #Writing systemparameters into builtin-module to make them globally callable
     #Overview given in Readme.txt
     
-    builtins.num_of_integration=rk4input[0]     
+    keys=list(rk4input.keys())
+    values=list(rk4input.values())
+    for i in range(len(keys)):
+        exec("builtins.%s=%f" % (keys[i],values[i]))
+    builtins.Runtime_Tracker=int
+    builtins.Noise_Tracker=0
+
+    """builtins.number_of_integration=rk4input['number_of_integration']     
     builtins.stepsize_of_integration=rk4input[1] 
-    builtins.latitude_stepsize=rk4input[2]      
-    builtins.latitude_NS=rk4input[3]
-    builtins.latitude_circle=rk4input[4]
-    builtins.latitude_belt=rk4input[5]
-    builtins.Tempstart_cosine=rk4input[6]
-    builtins.Tempstart_amplitude=rk4input[7]
-    builtins.Tempstart_noise=rk4input[8]
-    builtins.Tempstart_noiseamplitude=rk4input[9]    
+    builtins.spatial_resolution=rk4input[2]      
+    builtins.both_hemispheres=rk4input[3]
+    builtins.latitudinal_circle=rk4input[4]
+    builtins.latitudinal_belt=rk4input[5]
+    builtins.initials['initial_temperature_cosine']=rk4input[6]
+    builtins.initials['initial_temperature_amplitude']=rk4input[7]
+    builtins.initials['initial_temperature_noise']=rk4input[8]
+    builtins.initials['initial_temperature_noise_amplitude']=rk4input[9]    
     builtins.Runtime_Tracker=rk4input[10]
     builtins.Noise_Tracker=rk4input[11]
     builtins.Condition=rk4input[12]
     builtins.ConditionLength=rk4input[13]
     builtins.ConditionValue=rk4input[14]
-    builtins.ReadoutStep=rk4input[15]
-    builtins.NumberofExternals=rk4input[16]
+    builtins.data_readout=rk4input[15]
+    builtins.number_of_externals=rk4input[16]"""
 
-def Var_importer(var):
-
+def Var_importer(initials):
+    
     ###filling the running variables with values depending on the systemconfiguration in rk4input###
-    if latitude_stepsize==0:
+
+    if spatial_resolution==0:
         dim=0
         print('0D')
     else:
         dim=1
        #NS==True corresponds to southpole to northpole representation (180 Degrees)
-        if latitude_NS==True:    
+        if both_hemispheres==True:    
             Latrange=180
 
             #Checking if Temperature and Latitude is set on a latitudal circle (0°,10°,..if step=10)
             #or on a latitudinal belt and therefore right between the boundaries (5°,15°,..if step=10)
 
             #circle==True and belt==False says on the latitudinal circle
-            if latitude_circle==True and latitude_belt==False:      
-                var[3]=np.linspace(-90+latitude_stepsize,90-latitude_stepsize,int(Latrange/latitude_stepsize-1))
-                var[4]=np.linspace(-90,90-latitude_stepsize,int(Latrange/latitude_stepsize))+latitude_stepsize/2
-                var[1]=np.array([var[1]]*int(Latrange/latitude_stepsize-1))
+            if latitudinal_circle==True and latitudinal_belt==False:      
+                initials['latitude_c']=np.linspace(-90+spatial_resolution,90-spatial_resolution,int(Latrange/spatial_resolution-1))
+                initials['latitude_b']=np.linspace(-90,90-spatial_resolution,int(Latrange/spatial_resolution))+spatial_resolution/2
+                initials['zmt']=np.array([initials['zmt']]*int(Latrange/spatial_resolution-1))
                 #Checking if the Temperature for each latitude starts with the same value or a 
                 #cosine shifted value range
-                if Tempstart_cosine==True:
-                    var[1]=var[1]+Tempstart_amplitude*(cosd(var[3])-1)
+                if initials['initial_temperature_cosine']==True:
+                    initials['zmt']=initials['zmt']+initials['initial_temperature_amplitude']*(cosd(initials['latitude_c'])-1)
 
             #circle==False and belt==True say on the latitudinal belt
-            if latitude_circle==False and latitude_belt==True:
-                var[4]=np.linspace(-90+latitude_stepsize,90-latitude_stepsize,int(Latrange/latitude_stepsize-1))
-                var[3]=np.linspace(-90,90-latitude_stepsize,int(Latrange/latitude_stepsize))+latitude_stepsize/2
-                var[1]=np.array([var[1]]*int(Latrange/latitude_stepsize))
-                if Tempstart_cosine==True:
-                    if Tempstart_noise==True:
-                        z=[0]*len(var[3])
-                        for k in range(len(var[3])):
-                            z[k]=np.random.normal(0,Tempstart_noiseamplitude)
+            if latitudinal_circle==False and latitudinal_belt==True:
+                initials['latitude_b']=np.linspace(-90+spatial_resolution,90-spatial_resolution,int(Latrange/spatial_resolution-1))
+                initials['latitude_c']=np.linspace(-90,90-spatial_resolution,int(Latrange/spatial_resolution))+spatial_resolution/2
+                initials['zmt']=np.array([initials['zmt']]*int(Latrange/spatial_resolution))
+                if initials['initial_temperature_cosine']==True:
+                    if initials['initial_temperature_noise']==True:
+                        z=[0]*len(initials['latitude_c'])
+                        for k in range(len(initials['latitude_c'])):
+                            z[k]=np.random.normal(0,initials['initial_temperature_noise_amplitude'])
                     else: 
                         z=0
-                    var[1]=var[1]+Tempstart_amplitude*(cosd(var[3])-1)+lna(z)
+                    initials['zmt']=initials['zmt']+initials['initial_temperature_amplitude']*(cosd(initials['latitude_c'])-1)+lna(z)
 
         #Not from southpole to northpole rather equator to pole
         else:
             Latrange=90     
-            if latitude_circle==True and latitude_belt==False:
-                var[3]=np.linspace(0,90-latitude_stepsize,int(Latrange/latitude_stepsize))
-                var[4]=np.linspace(0,90-latitude_stepsize,int(Latrange/latitude_stepsize))+latitude_stepsize/2
-                var[1]=np.array([var[1]]*int(Latrange/latitude_stepsize))
-                if Tempstart_cosine==True:
-                    var[1]=var[1]+Tempstart_amplitude*(cosd(var[3])-1)
-            if latitude_circle==False and latitude_belt==True:
-                var[4]=np.linspace(0,90-latitude_stepsize,int(Latrange/latitude_stepsize))
-                var[3]=np.linspace(0,90-latitude_stepsize,int(Latrange/latitude_stepsize))+latitude_stepsize/2
-                var[1]=np.array([var[1]]*int(Latrange/latitude_stepsize))
-                if Tempstart_cosine==True:
-                    var[1]=var[1]+Tempstart_amplitude*(cosd(var[3])-1)
+            if latitudinal_circle==True and latitudinal_belt==False:
+                initials['latitude_c']=np.linspace(0,90-spatial_resolution,int(Latrange/spatial_resolution))
+                initials['latitude_b']=np.linspace(0,90-spatial_resolution,int(Latrange/spatial_resolution))+spatial_resolution/2
+                initials['zmt']=np.array([initials['zmt']]*int(Latrange/spatial_resolution))
+                if initials['initial_temperature_cosine']==True:
+                    initials['zmt']=initials['zmt']+initials['initial_temperature_amplitude']*(cosd(initials['latitude_c'])-1)
+            if latitudinal_circle==False and latitudinal_belt==True:
+                initials['latitude_b']=np.linspace(0,90-spatial_resolution,int(Latrange/spatial_resolution))
+                initials['latitude_c']=np.linspace(0,90-spatial_resolution,int(Latrange/spatial_resolution))+spatial_resolution/2
+                initials['zmt']=np.array([initials['zmt']]*int(Latrange/spatial_resolution))
+                if initials['initial_temperature_cosine']==True:
+                    initials['zmt']=initials['zmt']+initials['initial_temperature_amplitude']*(cosd(initials['latitude_c'])-1)
+    
+    Vars.t=initials['time']
+    Vars.T=initials['zmt']
+    Vars.T_global=initials['gmt']
+    Vars.Lat=initials['latitude_c']
+    Vars.Lat2=initials['latitude_b']
 
-
-def Output_importer(var):
+def Output_importer():
     #Assigning dynamical variables in Variables Package with initial values from var
-    Vars.t,Vars.T,Vars.T_global,Vars.Lat,Vars.Lat2=var
-    Vars.cL,Vars.C,Vars.F,Vars.v,Vars.P,Vars.Transfer,Vars.alpha,Vars.BudTransfer,Vars.Solar,Vars.Noise,Vars.Rin,Vars.Rout,Vars.ExternalOutput=    np.array([[0]*int(num_of_integration/ReadoutStep)]*len(Vars.Read),dtype=object)
-    Vars.ExternalOutput=np.array([Vars.ExternalOutput for i in range(NumberofExternals)],dtype=object)
-    Vars.External_time_start=np.array([0 for i in range(NumberofExternals)],dtype=object)
-    Vars.ForcingTracker=np.array([[0,0] for i in range(NumberofExternals)],dtype=object)
-    Vars.ExternalInput=np.array([0 for i in range(NumberofExternals)],dtype=object)
+    Vars.cL,Vars.C,Vars.F,Vars.v,Vars.P,Vars.Transfer,Vars.alpha,Vars.BudTransfer,Vars.Solar,Vars.Noise,Vars.Rin,Vars.Rout,Vars.ExternalOutput=    np.array([[0]*int(number_of_integration/data_readout)]*len(Vars.Read),dtype=object)
+    Vars.ExternalOutput=np.array([Vars.ExternalOutput for i in range(int(number_of_externals))],dtype=object)
+    Vars.External_time_start=np.array([0 for i in range(int(number_of_externals))],dtype=object)
+    Vars.ForcingTracker=np.array([[0,0] for i in range(int(number_of_externals))],dtype=object)
+    Vars.ExternalInput=np.array([0 for i in range(int(number_of_externals))],dtype=object)
     Vars.Read=[Vars.cL,Vars.C,Vars.F,Vars.v,Vars.P,Vars.Transfer,Vars.alpha,Vars.BudTransfer,               Vars.Solar,Vars.Noise,Vars.Rin,Vars.Rout,Vars.ExternalOutput]
 
 
