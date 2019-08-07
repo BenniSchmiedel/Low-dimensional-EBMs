5076a64909dfbd750072e370d7271a82bcd418ab
Benni <benny.schmiedel@gmail.com>
2019-08-07 18:14:20 +0200

    init fix 8

--- lowEBMs/Packages/Constants.py
+++ lowEBMs/Packages/Constants.py
@@ -12,14 +12,14 @@ Rd = 287.         # gas constant for dry air (J / kg / K)
 kappa = Rd / cp
 Rv = 461.5       # gas constant for water vapor (J / kg / K)
 cpv = 1875.   # specific heat at constant pressure for water vapor (J / kg / K)
-Omega = 2*np.pi/24./3600.  # Earth's rotation rate, (s^(-1))
+Omega = 2*3.14159/24./3600.  # Earth's rotation rate, (s^(-1))
 g = 9.8          # gravitational acceleration (m / s^2)
 kBoltzmann = 1.3806488E-23  # the Boltzmann constant (J / K)
 c_light = 2.99792458E8   # speed of light (m/s)
 hPlanck = 6.62606957E-34  # Planck's constant (J s)
 # sigma = 5.67E-8  # Stefan-Boltzmann constant (W / m^2 / K^4)
 #  sigma derived from fundamental constants
-sigma = (2*np.pi**5 * kBoltzmann**4) / (15 * c_light**2 * hPlanck**3)
+sigma = (2*3.14159**5 * kBoltzmann**4) / (15 * c_light**2 * hPlanck**3)
 
 S0 = 1366.14      # solar constant (W / m^2)
 
