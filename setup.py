1128b0bdb06c29accba4afd2b0b444aec24df8a0
Benjamin Schmiedel <bsc029@gfi3100245.klientdrift.uib.no>
2019-08-23 14:15:09 +0200

    readme included in setup

--- setup.py
+++ setup.py
@@ -4,11 +4,16 @@ import setuptools
 #def readme():
 #    with open('README.rst') as f:
 #        return f.read()
-
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.txt'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(name='lowEBMs',
-      version='0.7',
+      version='0.7.1',
       description='A python implementation of low-dimensional EBMs',
+      long_description=long_description,
+      long_description_content_type='text/plain'
       url='https://github.com/BenniSchmiedel/Low-dimensional-EBMs',
       author='Benjamin Schmiedel',
       license='MIT',
